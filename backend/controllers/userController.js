1a7a3f6e23154ee55db46ac01c77d89c754e8ba0
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-04-27 13:31:26 +0100

    No changes yet here

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,7 +34,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (userExists) {
     res.status(400); //Bad request
-    throw new Error("User already exists");
+    throw new Error("User already existss");
   }
   const user = await User.create({
     name,
